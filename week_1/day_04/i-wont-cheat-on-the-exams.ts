8e84ba612fae83787fa689f085a8c6aa4f4afe33
juditvarro <dr.judit.varro@gmail.com>
2018-08-03 11:29:20 +0200

    i-wont-cheat-on-the-exams.ts finished

---
+++ week_1/day_04/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let examLine: string = '\"I won\'t cheat on the exam!\"';
+
+for (let j: number = 0; j < 100; j++) {
+    console.log(examLine);
+}
