c7a23f30224b8b44c0514bbc206a872c9f757ffd
juditvarro <dr.judit.varro@gmail.com>
2018-08-05 19:48:14 +0200

    Draw-pyramid.ts finished.

---
+++ week_1/day_04/draw-pyramid.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+let lineCount: number = 4;
+let spaceCount: number = lineCount - 2;
+let starCount: number = lineCount + 2;
+let space: string = ' ';
+let star: string = '*';
+
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+
+for (let i: number = 0; i <= lineCount; i++) {
+    for (let j: number = i; j <= lineCount; j++) {
+        space += ' ';
+    }
+    console.log(space + star);
+    star += '**';
+    space = ' ';
+}
