d56f0833aa5fadb8b32af833ebf631adc65208c6
juditvarro <dr.judit.varro@gmail.com>
2018-08-06 16:36:24 +0200

    Tasks completed includint compare-length.ts.

---
+++ week_2/day_01/compare-length.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+export {};
+
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
+
+let firstList = [1, 2, 3];
+let secondList = [4, 5];
+
+if (secondList.length > firstList.length) {
+    console.log('Yes, it has more elements.');}
+else {
+    console.log('Nope, the second list is shorter.');    
+}
