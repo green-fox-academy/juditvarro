37d38cf26b26ed6e806925cadb39122d0954af8a
juditvarro <dr.judit.varro@gmail.com>
2018-08-06 13:48:29 +0200

    New diagonal and square method.

--- week_1/day_04/draw-diagonal.ts
+++ week_1/day_04/draw-diagonal.ts
@@ -2,8 +2,6 @@
 
 let lineCount: number = 6;
 
-let row: string;
-let place: string;
 
 // Write a program that draws a
 // square like this:
@@ -17,6 +15,7 @@ let place: string;
 //
 // The square should have as many lines as lineCount is
 
+/*
 for (let i: number = 0; i < lineCount; i++) {
     if (i == 0 || i == lineCount - 1) {
         for (row = '%'; row.length < lineCount; row += place) {        
@@ -31,4 +30,20 @@ for (let i: number = 0; i < lineCount; i++) {
             }
     }
  console.log(row);
+}*/
+
+let row: string = '';
+let place: string = ' ';
+let symbol: string = '%';
+
+for (let i: number = 1; i <= lineCount; i++) {
+        for (let j: number = 1; j <= lineCount; j++) {
+                if (i == 1 || i == lineCount || j == 1 || j == lineCount || i == j) {
+                    row += symbol;
+                } else {
+                    row += place;
+                }
+        } 
+        console.log(row);
+        row = '';
 }
--- week_1/day_04/draw-square.ts
+++ week_1/day_04/draw-square.ts
@@ -1,9 +1,7 @@
 'use strict';
 
-let lineCount: number = 6;
 
-let body: string = "%    %";
-let row: string = "%%%%%%";
+let lineCount: number = 6;
 
 // Write a program that draws a square like this:
 //
@@ -16,12 +14,18 @@ let row: string = "%%%%%%";
 //
 // The square should have as many lines as lineCount is
 
+let row: string = '';
+let place: string = ' ';
+let symbol: string = '%';
 
-
-for (let i: number = 0; i <= lineCount -1; i++) {
-    if (i === 0 || i === lineCount - 1) {
+for (let i: number = 1; i <= lineCount; i++) {
+        for (let j: number = 1; j <= lineCount; j++) {
+                if (i == 1 || i == lineCount || j == 1 || j == lineCount) {
+                    row += symbol;
+                } else {
+                    row += place;
+                }
+        } 
         console.log(row);
-    } else {
-        console.log(body);
-    }
+        row = '';
 }
