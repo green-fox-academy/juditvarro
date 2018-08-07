086beda6d58efa73be28372eb7f46713eae95358
juditvarro <dr.judit.varro@gmail.com>
2018-08-07 15:07:11 +0200

    Matchmaking.ts finished.

--- week_2/day_02/matchmaking.ts
+++ week_2/day_02/matchmaking.ts
@@ -6,6 +6,25 @@
 let girls: string[] = ['Eve', 'Ashley', 'Claire', 'Kat', 'Jane'];
 let boys: string[] = ['Joe', 'Fred', 'Tom', 'Todd', 'Neef', 'Jeff'];
 
+function makingMatches (input1: string[], input2: string[]) {
+   let numberOfElements: number;
+   let commonList: string[] = [];
+        if
+        (input1.length > input2.length) {
+            numberOfElements = input2.length;
+        } else if 
+        (input2.length > input1.length) {
+            numberOfElements = input1.length;
+        } else {
+                numberOfElements = input1.length;
+            }
+    for (let i = 0; i <= numberOfElements; i++) {
+        commonList.push(input1[i]);
+        commonList.push(input2[i]);
+    }
+    return commonList;
+}
+
 console.log(makingMatches(girls, boys));
 
 export = makingMatches;
