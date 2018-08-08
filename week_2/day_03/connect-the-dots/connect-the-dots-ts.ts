2e7ce0d7f8ef45a64c3926d517547e3f1ff358c8
juditvarro <dr.judit.varro@gmail.com>
2018-08-09 00:07:03 +0200

    Connect-the-dots-ts.ts is finished.

--- week_2/day_03/connect-the-dots/connect-the-dots-ts.ts
+++ week_2/day_03/connect-the-dots/connect-the-dots-ts.ts
@@ -11,6 +11,61 @@ const ctx = canvas.getContext('2d');
 // Connect these: [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70],
 // [120, 100], [85, 130], [50, 100]]
 
-function connecter(input: number[]) {
-    
+// Task 1 -- Square
+/*
+let arrayOfDots: number[][] = [
+    [10, 10],
+    [290, 10],
+    [290, 290],
+    [10, 290]
+];
+
+function connecter(input: number[][]) {
+
+    for (let i: number = 0; i < input.length; i++) {
+        if (i == input.length - 1) {
+            ctx.beginPath();
+            ctx.strokeStyle = 'green';
+            ctx.moveTo(input[i][0], input[i][1]);
+            ctx.lineTo(input[0][0], input[0][1]);
+            ctx.stroke();
+        } else {
+            ctx.beginPath();
+            ctx.strokeStyle = 'green';
+            ctx.moveTo(input[i][0], input[i][1]);
+            ctx.lineTo(input[i + 1][0], input[i + 1][1]);
+            ctx.stroke();
+        }
+    }
 }
+
+connecter(arrayOfDots);
+*/
+
+// Task 2 -- A GREEN FOX!
+
+let arrayOfDots: number[][] = [
+    [50, 100], [70, 70], [80, 90], [90, 90], [100, 70],
+    [120, 100], [85, 130], [50, 100]
+];
+
+function connecter(input: number[][]) {
+
+    for (let i: number = 0; i < input.length; i++) {
+        if (i == input.length - 1) {
+            ctx.beginPath();
+            ctx.strokeStyle = 'green';
+            ctx.moveTo(input[i][0], input[i][1]);
+            ctx.lineTo(input[0][0], input[0][1]);
+            ctx.stroke();
+        } else {
+            ctx.beginPath();
+            ctx.strokeStyle = 'green';
+            ctx.moveTo(input[i][0], input[i][1]);
+            ctx.lineTo(input[i + 1][0], input[i + 1][1]);
+            ctx.stroke();
+        }
+    }
+}
+
+connecter(arrayOfDots);
