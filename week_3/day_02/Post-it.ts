ec5d691f56c67675f582b364a8e3d1b5cd7ef038
juditvarro <dr.judit.varro@gmail.com>
2018-08-15 14:03:42 +0200

    Post-it.ts finished

---
+++ week_3/day_02/Post-it.ts
@@ -0,0 +1,14 @@
+class PostIt{
+    backgroundcolor: string;
+    text: string;
+    textColor: string;
+    constructor(backgroundcolor: string, text: string, textColor: string){
+        this.backgroundcolor = backgroundcolor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+}
+
+let orangePostIt: PostIt = new PostIt('orange', 'Idea 1', 'blue');
+let pinkPostIt: PostIt = new PostIt('pink', 'Awesome', 'black');
+let yellowPostIt: PostIt = new PostIt('yellow', 'Superb!', 'green');
