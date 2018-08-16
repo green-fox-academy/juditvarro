3e02dd2a1384ecf9a5baa5ae834a74eee929c0eb
juditvarro <dr.judit.varro@gmail.com>
2018-08-16 11:27:22 +0200

    tried to fix counter.ts without inner log (but didn't work).

--- week_3/day_03/counter.ts
+++ week_3/day_03/counter.ts
@@ -1,14 +1,17 @@
 // Write a recursive function that takes one parameter: n and counts down from n.
 
-function subtraction(maximumNumber: number) {
-  if (maximumNumber <= 0) {
+'use strict'
+export { };
+
+function subtraction(maximumNumber: number): number {
+  if (maximumNumber < 0) {
     throw new Error('Please provide a positive integer.')
   } else {
-    if (maximumNumber > 0) {
+    if (maximumNumber == 0) {
       console.log(maximumNumber);
-      subtraction(maximumNumber - 1);
     } else {
       console.log(maximumNumber);
+      return subtraction(maximumNumber - 1);
     }
   }
 }
---
+++ week_3/day_03/number-adder.ts
@@ -0,0 +1,5 @@
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+'use strict'
+export { };
+
