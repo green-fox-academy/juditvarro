f39d9859bf7ade9f858a6cb24a42d0b689dade5b
juditvarro <dr.judit.varro@gmail.com>
2018-08-16 13:46:23 +0200

    Sum-digit.ts finished with recursive function.

---
+++ week_3/day_03/sum-digit.ts
@@ -0,0 +1,14 @@
+// Given a non-negative int n, return the sum of its digits recursively (no loops). 
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+function addedDigits(numToCount: number): number {
+    let remainder: number = numToCount % 10
+    if ((numToCount - remainder) !=0 ) {
+        return remainder += addedDigits((numToCount - remainder) / 10);
+    } else {
+        return remainder;
+    }
+}
+
+console.log(addedDigits(2002));
