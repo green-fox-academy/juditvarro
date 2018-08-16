1137c2ae40d047379fb19953248832d803afdb45
juditvarro <dr.judit.varro@gmail.com>
2018-08-16 19:44:54 +0200

    string3.ts finished with simple recursive function.

--- week_3/day_03/string3.ts
+++ week_3/day_03/string3.ts
@@ -1,3 +1,14 @@
 // Given a string, compute recursively a new string where all the
 // adjacent chars are now separated by a '*'.
 
+let newString: string = 'This will be nice'
+
+function starAdder(text: string): string {
+    if (text.length === 1) {
+        return text;
+    } else {
+        return text[0] + '*' + starAdder(text.slice(1));
+    }
+}
+
+console.log(starAdder(newString));
