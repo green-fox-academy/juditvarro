9c68f90dd5b1b3ff83c3e8608920d01e0a8c6d74
juditvarro <dr.judit.varro@gmail.com>
2018-09-14 11:39:00 +0200

    feat(galleryproject): added style.css and content is now centered with arrows

--- week_6/day_05/galleryproject/index.js
+++ week_6/day_05/galleryproject/index.js
@@ -1,15 +1,15 @@
 'use strict'
 
 const express = require('express');
-const path = require('path');
 const app = express();
 const PORT = 3030;
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
   res.render('home', {
-    message: 'pong',
+    picture: 'https://images.theconversation.com/files/64453/original/js7ptn5n-1415855719.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip',
   });
 });
 
