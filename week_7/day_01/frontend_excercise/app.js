7afa13076290b82c54be4aee82fccbb5cabb378c
juditvarro <dr.judit.varro@gmail.com>
2018-09-17 12:13:58 +0200

    Feat(frontend_excercixe) added path and app.js uses static asset elements

--- week_7/day_01/frontend_excercise/app.js
+++ week_7/day_01/frontend_excercise/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+
+const path = require('path');
 const PORT = 3060;
 
+app.use('/assets', express.static('assets'));
+
 app.get('/', (req, res) => {
-  res.send('OK!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.listen(PORT, () => {
   console.log(`App successfully started and running on ${PORT}.`);
-  
 })
