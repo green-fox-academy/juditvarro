d1f9184b0dfd116497e4ee04279e5559606c8746
juditvarro <dr.judit.varro@gmail.com>
2018-09-17 12:38:37 +0200

    Feat(frontend_excercise) new doubling endpoint

--- week_7/day_01/frontend_excercise/app.js
+++ week_7/day_01/frontend_excercise/app.js
@@ -1,13 +1,28 @@
+'use strict'
+
 const express = require('express');
 const app = express();
 
 const path = require('path');
-const PORT = 3060;
+const PORT = 8080;
 
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.get('/doubling', (req, res) => {
+  if (req.query.input) {
+    res.json({
+      "received": req.query.input,
+      "result": req.query.input * 2,
+    })
+  } else {
+    res.json({
+    error: "Please provide an input!"
+    })
+  }
 })
 
 app.listen(PORT, () => {
