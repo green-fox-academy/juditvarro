fa2f4c41288cc20236edce0392a3cdb72646dc9b
juditvarro <dr.judit.varro@gmail.com>
2018-09-22 16:46:02 +0200

    Feat(giphyapi) added css elements and two more buttons to hide or show all (function not added yet)

--- week_7/day_02/giphyapi/assets/script.js
+++ week_7/day_02/giphyapi/assets/script.js
@@ -2,12 +2,28 @@ window.onload = () => {
   const host = 'http://localhost:3060';
   const http = new XMLHttpRequest();
   const giphyParent = document.querySelector('#giphys');
+  const buttonParent = document.querySelector('#showallbuttons');
   http.open('GET', `${host}/api/giphy/alpaca`, true);
   console.log(http);
 
   http.onload = () => {
     if (http.status === 200) {
       const giphys = JSON.parse(http.response).data;
+
+      let showclass = document.createElement('div');
+      let hideclass = document.createElement('div');
+      let showAll = document.createElement('button');
+      showAll.className = "showall";
+      showAll.innerText = "Please show them all, I cannot wait!"
+      let hideAll = document.createElement('button');
+      hideAll.className = "hideall";
+      hideAll.innerText = "Please hide them, too much cuteness!"
+
+      showclass.appendChild(showAll);
+      hideclass.appendChild(hideAll);
+      buttonParent.appendChild(showclass);
+      buttonParent.appendChild(hideclass);
+
       giphys.forEach(element => {
         let content = document.createElement('div');
         content.className = 'giphypack';
@@ -25,6 +41,10 @@ window.onload = () => {
           } else {
             gifImg.className = "clear";
           }
+        }
+
+        showAll.onclick = () => {
+          
         }
 
         gifButton.appendChild(gifImg);
