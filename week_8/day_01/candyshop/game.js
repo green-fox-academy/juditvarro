0acd8f8543194be9ba894a49ee3a71b4bf41435d
juditvarro <dr.judit.varro@gmail.com>
2018-09-26 10:52:02 +0200

    Feat(w8d02) mediaquery and fancy-table tasks finished

--- week_8/day_01/candyshop/game.js
+++ week_8/day_01/candyshop/game.js
@@ -1,55 +1,115 @@
-// Gather 10.000 candies!
-// Clicking the ‘Create candies’ button gives you 1 candy.
-// You can buy a lollipop for 100 candies by clicking the ‘Buy lollipop’ button.
-// 10 lollipops generate 1 candy per second.
-// Use the 🍭 emoji to display the lollipops you have
-// Display the candy producton rate in the Candies / Second row
-// If you press the "make candy rain" button, the candy generation should speed up 10x
+// // Gather 10.000 candies!
+// // Clicking the ‘Create candies’ button gives you 1 candy.
+// // You can buy a lollipop for 100 candies by clicking the ‘Buy lollipop’ button.
+// // 10 lollipops generate 1 candy per second.
+// // Use the 🍭 emoji to display the lollipops you have
+// // Display the candy producton rate in the Candies / Second row
+// // If you press the "make candy rain" button, the candy generation should speed up 10x
 
-let candies = document.querySelector(".candies");
-let lollypops = document.querySelector(".lollypops");
-let speed = document.querySelector(".speed");
+const candies = document.querySelector(".candies");
+const lollypops = document.querySelector(".lollypops");
+const speed = document.querySelector(".speed");
 
-let createCandies = document.querySelector(".create-candies");
-let buyLollypops = document.querySelector(".buy-lollypops");
-let candyMachine = document.querySelector(".candy-machine");
+const createCandies = document.querySelector(".create-candies");
+const buyLollypops = document.querySelector(".buy-lollypops");
+const candyMachine = document.querySelector(".candy-machine");
 
-const gimmeCandy = (event) => {
-  let startingCandies = parseInt(candies.innerHTML);
-  candies.innerHTML = `${startingCandies + 1}`;
-}
-
-const buyLollypop = (event) => {
-  let startingCandies = parseInt(candies.innerHTML);
-  let startingLollypops = lollypops.innerHTML;
-  if (startingCandies >= 100) {
-    candies.innerHTML = `${startingCandies - 100}`;
-    startingLollypops += "🍭";
-    lollypops.innerHTML = startingLollypops;
-  }
+const gimmeCandy = () => {
+  let startingCandies = parseInt(candies.innerText);
+  candies.innerText = `${startingCandies + 1}`;
+  startingCandies = parseInt(candies.innerText);
 }
 
 const candyGenerator = () => {
-  let startingLollypops = lollypops.innerHTML;
-  let starter = startingLollypops.length / 2;
-  let startingCandies = parseInt(candies.innerHTML);
-  if (starter >= 10) {
-    candies.innerHTML = `${startingCandies + 1}`;
+  let startingLollypops = lollypops.innerText;
+  let currentLollypopNum = startingLollypops.length / 2;
+  if (currentLollypopNum >= 10) {
+    gimmeCandy();
   }
 }
 
-const speeder = () => {
-  let currentLolly = (lollypops.innerHTML).length / 2;
-  let ratio = 1000 / (currentLolly / 10);
-  return ratio;
+function candyGeneratorTimed(input) {
+  setInterval(function () { candyGenerator() }, input * 1000);
 }
 
-speed.innerHTML = speeder() / 1000;
+const buyLollypop = () => {
+  let startingCandies = parseInt(candies.innerText);
+  let startingLollypops = lollypops.innerText;
+  let currentLollypopNum = startingLollypops.length / 2;
+  let lollyMakerRatioSimple = currentLollypopNum / 10;
 
-const candyGeneratorTimed = (function() {setInterval(function() {candyGenerator()}, speeder())});
+  if (startingCandies >= 100 && startingCandies <= 10000) {
+    candies.innerText = `${startingCandies - 100}`;
+    startingLollypops += "🍭";
+    lollypops.innerText = startingLollypops;
+  }
+  if (currentLollypopNum >= 8 && startingCandies <= 10000) {
+    candyGeneratorTimed(lollyMakerRatioSimple);
+    getSpeedRatio();
+  }
+}
+
+function getSpeedRatio(){
+  let startingLollypops = lollypops.innerText;
+  let currentLollypopNum = startingLollypops.length / 2;
+  let currentRatio = currentLollypopNum / 10;
+  speed.innerText = currentRatio;
+}
 
 createCandies.addEventListener("click", gimmeCandy);
 buyLollypops.addEventListener("click", buyLollypop);
-candyGeneratorTimed();
+
+candyMachine.addEventListener("click", function () {
+  let startingLollypops = lollypops.innerText;
+  let currentLollypopNum = startingLollypops.length / 2;
+  let lollyMakerRatioFast = currentLollypopNum / 100;
+  candyGeneratorTimed(lollyMakerRatioFast);
+  getSpeedRatio();
+})
 
 
+// let candies = document.querySelector(".candies");
+// let lollypops = document.querySelector(".lollypops");
+// let speed = document.querySelector(".speed");
+
+// let createCandies = document.querySelector(".create-candies");
+// let buyLollypops = document.querySelector(".buy-lollypops");
+// let candyMachine = document.querySelector(".candy-machine");
+
+// const gimmeCandy = (event) => {
+//   let startingCandies = parseInt(candies.innerHTML);
+//   candies.innerHTML = `${startingCandies + 1}`;
+// }
+
+// const buyLollypop = (event) => {
+//   let startingCandies = parseInt(candies.innerHTML);
+//   let startingLollypops = lollypops.innerHTML;
+//   if (startingCandies >= 100) {
+//     candies.innerHTML = `${startingCandies - 100}`;
+//     startingLollypops += "🍭";
+//     lollypops.innerHTML = startingLollypops;
+//   }
+// }
+
+// const candyGenerator = () => {
+//   let startingLollypops = lollypops.innerHTML;
+//   let starter = startingLollypops.length / 2;
+//   let startingCandies = parseInt(candies.innerHTML);
+//   if (starter >= 10) {
+//     candies.innerHTML = `${startingCandies + 1}`;
+//   }
+// }
+
+// const speeder = () => {
+//   let currentLolly = (lollypops.innerHTML).length / 2;
+//   let ratio = 1000 / (currentLolly / 10);
+//   return ratio;
+// }
+
+// speed.innerHTML = speeder() / 1000;
+
+// const candyGeneratorTimed = (function() {setInterval(function() {candyGenerator()}, speeder())});
+
+// createCandies.addEventListener("click", gimmeCandy);
+// buyLollypops.addEventListener("click", buyLollypop);
+// candyGeneratorTimed();
