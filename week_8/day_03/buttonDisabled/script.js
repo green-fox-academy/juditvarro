a1702363f6de2697ea0fba3dd770dc4d72770772
juditvarro <dr.judit.varro@gmail.com>
2018-09-27 17:15:54 +0200

    Feat(disabledButtons): addeventlistener for change event works

---
+++ week_8/day_03/buttonDisabled/script.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+
+  const form = document.querySelector("#formhandler");
+  const firstButtons = document.querySelector(".first_set");
+  const secondButtons = document.querySelector(".second_set");
+  const simpleButtons = document.querySelector("button");
+
+  firstButtons.addEventListener("change", (event) => {
+    alert('it works!');
+  })
+
+  
+}
