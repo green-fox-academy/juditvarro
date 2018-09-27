26acebeda7107e3ae30f278afa4c94433a31c5d4
juditvarro <dr.judit.varro@gmail.com>
2018-09-27 17:46:20 +0200

    Feat(disabledButtons): submitbuttons with alerts also added

--- week_8/day_03/buttonDisabled/script.js
+++ week_8/day_03/buttonDisabled/script.js
@@ -3,11 +3,29 @@ window.onload = () => {
   const form = document.querySelector("#formhandler");
   const firstButtons = document.querySelector(".first_set");
   const secondButtons = document.querySelector(".second_set");
-  const simpleButtons = document.querySelector("button");
+  const submitButton = document.querySelector("button[type=submit]");
+  const catButton = document.querySelector("button[type=button]");
 
   firstButtons.addEventListener("change", (event) => {
-    alert('it works!');
+    if (event.target.id === "dog" || event.target.id === "cat") {
+      submitButton.disabled = false;
+    }
+  })
+
+  secondButtons.addEventListener("change", (event) => {
+    if (event.target.id === "yes") {
+      catButton.disabled = false;
+    }
   })
 
   
+
+
+  submitButton.addEventListener("click", (event) => {
+    alert("Thank you, you have successfully signed up!")
+  })
+
+  catButton.addEventListener("click", (event) => {
+    alert("Thank you, you've successfully signed up for cat facts!")
+  })
 }
